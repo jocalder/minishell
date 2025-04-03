3316c9bea6fced89fd36a1ccee6604b2933c4e9a
victor-goyzueta <vgoyzuet@c2r10s3.42madrid.com>
2025-04-03 18:41:12 +0200

    test

--- src/mini_signal_handler.c
+++ src/mini_signal_handler.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   mini_signal_handler.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/03 18:28:13 by vgoyzuet          #+#    #+#             */
-/*   Updated: 2025/04/03 18:39:00 by vgoyzuet         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
 void	mini_signal_handler(int signum, siginfo_t *info, void *unused)
