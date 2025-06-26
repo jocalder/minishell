5982b9a0696a8f476ad16b0474e9107abfe5f05e
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-06-26 16:29:50 +0200

    normitte

--- src/set_input.c
+++ src/set_input.c
@@ -23,9 +23,8 @@ int	split_cmd(t_cmd **cmd)
 			return (free(new->value), free(new), g_status);
 		type = get_type((*cmd)->token, new->value, new->flag);
 		append_token(*cmd, &new, type);
-		// Here or at execution?
-		// if (!is_supported(new->value))
-		// 	return (w_unsupported(new->value), update_status(SYNTAX));
+		if (!is_supported(new->value))
+			return (w_unsupported(new->value), update_status(SYNTAX));
 	}
 	return (OK);
 }
