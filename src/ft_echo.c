2f453b2051203cf1c9a52e45486eddd286c9d243
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-06-26 17:35:35 +0200

    update

--- src/ft_echo.c
+++ src/ft_echo.c
@@ -1,55 +1,24 @@
 #include "minishell.h"
 
-// int	ft_echo(t_cmd *cmd)
-// {
-// 	bool	suppress_newline;
-
-// 	suppress_newline = false;
-// 	cmd->token = cmd->token->next;
-// 	while (cmd->token)
-// 	{
-// 		if (cmd->token->type == ARG)
-// 		{
-// 			if (ft_strncmp(cmd->token->value, "-n", 3) == 0)
-// 				suppress_newline = true;
-// 			else if (suppress_newline)
-// 				break ;
-// 		}
-// 		cmd->token = cmd->token->next;
-// 	}
-// 	while (cmd->token && cmd->token->type == ARG)
-// 	{
-// 		write(STDOUT_FILENO, cmd->token->value, ft_strlen(cmd->token->value));
-// 		if (cmd->token->next && cmd->token->next->type == ARG)
-// 			write(STDOUT_FILENO, " ", 1);
-// 		cmd->token = cmd->token->next;
-// 	}
-// 	if (!suppress_newline)
-// 		write(STDOUT_FILENO, "\n", 1);
-// 	return (update_status(OK));
-// }
-
 int	ft_echo(t_cmd *cmd)
 {
 	bool	suppress_newline;
 
 	suppress_newline = false;
 	cmd->token = cmd->token->next;
-	while (cmd->token && cmd->token->type == ARG)
+	while (cmd->token && cmd->token->type != ARG)
+		cmd->token = cmd->token->next;
+	while (cmd->token && (cmd->token->type == ARG
+		&& ft_strncmp(cmd->token->value, "-n", 3) == 0))
 	{
-		if (ft_strncmp(cmd->token->value, "-n", 3) != 0)
-			break ;
 		suppress_newline = true;
 		cmd->token = cmd->token->next;
 	}
-	while (cmd->token)
+	while (cmd->token && cmd->token->type == ARG)
 	{
-		if (cmd->token->type == ARG)
-		{
-			write(1, cmd->token->value, ft_strlen(cmd->token->value));
-			if (cmd->token->next && cmd->token->next->type == ARG)
-				write(STDOUT_FILENO, " ", 1);
-		}
+		write(STDOUT_FILENO, cmd->token->value, ft_strlen(cmd->token->value));
+		if (cmd->token->next && cmd->token->next->type == ARG)
+			write(STDOUT_FILENO, " ", 1);
 		cmd->token = cmd->token->next;
 	}
 	if (!suppress_newline)
