ad7a9fce75c80f918777135342dc823766406fe3
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-06-27 05:05:22 +0200

    update

--- src/builtins.c
+++ src/builtins.c
@@ -1,39 +1,5 @@
 #include "minishell.h"
 
-// static void	ft_cd(t_mini *data)
-// {
-// 	char	*home;
-
-// 	home = NULL;
-// 	if (!data->args[1])
-// 	{
-// 		home = getenv("HOME");
-// 		if (!home)
-// 			perror("minishell: cd: HOME not set\n");
-// 		if (chdir(home) != 0)
-// 			perror("error home");//change for a similar error message
-// 	}
-// 	else
-// 	{
-// 		if (chdir(data->args[1]) != 0)
-// 			perror("error route");//change for a similar error message
-// 	}
-// }
-
-// static void	ft_env(char **envp)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if (!envp)
-// 		return ;
-// 	while (envp[i])
-// 	{
-// 		printf("%s\n", envp[i]);
-// 		i++;
-// 	}
-// }
-
 void	w_builtin_usage(char *builtin, char *invalid_opt)
 {
 	write(STDERR_FILENO, "minishell: ", 12);
