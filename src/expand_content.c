1cfdfe8c9b9b138ae42e4bb8f7edb395cfda0a62
Víctor J. Goyzueta <167643295+victor-goyzueta@users.noreply.github.com>
2025-07-02 16:02:33 +0200

    update expand_content

--- src/expand_content.c
+++ src/expand_content.c
@@ -27,7 +27,8 @@ static char	*handler_expand(char *value, size_t *len)
 	else if (ft_isalpha(value[*len]) || value[*len] == '_')
 	{
 		while (value[*len]
-			&& (value[*len] != '$' && !is_spacetab(value[*len])))
+			&& (value[*len] != '$' && value[*len] != '='
+				&& !is_spacetab(value[*len])))
 			(*len)++;
 		new_value = get_env_var(ft_substr(value, 0, *len));
 	}
@@ -57,7 +58,7 @@ char	*expand_content(char *value, t_token *last)
 		tmp = NULL;
 		len = 0;
 		if (*start == '$' && (start[len + 1]
-			&& (!is_spacetab(start[len + 1]) && start[len + 1] != '=')))
+				&& (!is_spacetab(start[len + 1]) && start[len + 1] != '=')))
 			tmp = handler_expand(++start, &len);
 		else
 			tmp = ft_substr(start, 0, ++len);
